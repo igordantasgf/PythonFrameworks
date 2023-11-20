71470cb1423ca2e7447a234e9bdbd9a73f5ffdbc
igordantasgf <igordantasgf@gmail.com>
2023-11-20 05:20:38 -0300

    tested GET methods and db connection

--- Flask+PHP/api/main/__init__.py
+++ Flask+PHP/api/main/__init__.py
@@ -1,8 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 import os
-from models import Comentario
-from main.response import gera_response
+from response import gera_response
 
 
 PASSWORD = os.environ.get('POSTGRES_PASSWORD')
@@ -11,6 +10,14 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://default:{}@ep-autumn-dream-20888543.us-east-1.postgres.vercel-storage.com:5432/verceldb'.format(PASSWORD)
 db = SQLAlchemy(app)
+
+# models
+class Comentarios(db.Model):
+    id = db.Column(db.Integer, primary_key= True)
+    mensagem = db.Column(db.String(60))
+
+    def to_json(self):
+        return {"mensagem": self.mensagem}
 
 
 @app.route("/")
@@ -21,7 +28,7 @@ def home():
 # Selecionar Tudo
 @app.route("/comentarios", methods=["GET"])
 def seleciona_comentarios():
-    comentarios_objetos = Comentario.query.all()
+    comentarios_objetos = Comentarios.query.all()
     comentarios_json = [comentario.to_json() for comentario in comentarios_objetos]
 
     return gera_response(200, "comentarios", comentarios_json)
--- Flask+PHP/api/main/models.py
+++
@@ -1,8 +0,0 @@
-from main import db
-
-# models
-class Comentario(db.Model):
-    mensagem = db.Column(db.String(60))
-
-    def to_json(self):
-        return {"mensagem": self.mensagem}
