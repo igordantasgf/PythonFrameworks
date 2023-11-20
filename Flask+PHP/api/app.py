3c045d2187ec66e840dd080f9f30aa484871c488
igordantasgf <igordantasgf@gmail.com>
2023-11-20 05:24:50 -0300

    .

--- Flask+PHP/api/app.py
+++ Flask+PHP/api/app.py
@@ -1,6 +1,6 @@
-from flask import Flask
+from flask import Flask, Response
 from flask_sqlalchemy import SQLAlchemy
-import os
+import os, json
 from response import gera_response
 
 
@@ -32,6 +32,17 @@ def seleciona_comentarios():
     comentarios_json = [comentario.to_json() for comentario in comentarios_objetos]
 
     return gera_response(200, "comentarios", comentarios_json)
+
+
+def gera_response(status, nome_do_conteudo, conteudo, mensagem=False):
+    body = {}
+    body[nome_do_conteudo] = conteudo
+
+    if(mensagem):
+        body["mensagem"] = mensagem
+
+    return Response(json.dumps(body), status=status, mimetype="application/json")
+
 
 if __name__ == "__main__":
     app.run(debug=True) 
